a04400d6410b437972807b064b01ba9a07087965
ekllerr <eklerzzy@gmail.com>
2025-08-30 17:35:56 +0200

    pressing keys updates current combo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
 import GameContainer from "./components/GameContainer.tsx";
+import {useEffect} from "react";
+import useGameStore from "./store/gameStore.ts";
 
 function App() {
+
+    useEffect(() => {
+        const store = useGameStore.getState();
+
+        const handleKeyDown = (e: KeyboardEvent) => {
+            const key = e.key.toLowerCase();
+
+            const orbEntry = Object.entries(store.orbBindings).find(([, bindKey]) => bindKey === key);
+
+            if (orbEntry) {
+                const [orbName] = orbEntry;
+                store.addOrbToCombo(orbName);
+            }
+
+        }
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => window.removeEventListener('keydown', handleKeyDown);
+
+    })
 
   return (
     <div className="flex flex-col items-center justify-center w-full">
